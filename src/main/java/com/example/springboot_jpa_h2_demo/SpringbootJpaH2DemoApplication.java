60b4e43d5546a024f33079860d5d1c3d624ae4e8
watermelon <laimin@trustkernel.com>
2018-12-06 15:50:08 +0800

    add springboot

---
+++ src/main/java/com/example/springboot_jpa_h2_demo/SpringbootJpaH2DemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.springboot_jpa_h2_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaH2DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJpaH2DemoApplication.class, args);
+    }
+}
